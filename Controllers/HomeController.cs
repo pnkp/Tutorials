202579ae33dd430544fa4c44e695a3cdce47eeea
pnkp <p.antyporowicz@gmail.com>
2019-04-07 19:12:35 +0200

    added actions and views

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,10 +1,39 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ViewsProject.Controllers
 {
     public class HomeController : Controller
     {
-        // GET
-        public ViewResult Index() => 
+        public ViewResult Index()
+        {
+            ViewBag.Controller = nameof(HomeController);
+            ViewBag.Action = nameof(Index);
+            return View("Views/Test/Test.cshtml");
+        }
+
+        public ViewResult Action()
+        {
+            var dict = new Dictionary<string, string>
+            {
+                ["Controller"] = nameof(HomeController),
+                ["Action"] = nameof(Action)
+            };
+            return View(dict);
+        }
+
+        public ViewResult List()
+        {
+            ViewBag.Controller = nameof(HomeController);
+            ViewBag.Action = nameof(List);
+            return View("Index");
+        }
+
+        public ViewResult Shared()
+        {
+            ViewBag.Controller = nameof(HomeController);
+            ViewBag.Action = nameof(List);
+            return View("SharedView");
+        }
     }
 }
