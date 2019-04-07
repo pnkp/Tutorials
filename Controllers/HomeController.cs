cb400c96eb26357f73bb4d4e530f15dfa1b6d7f2
pnkp <p.antyporowicz@gmail.com>
2019-04-07 08:12:55 +0200

    init commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ViewsProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET
+        public ViewResult Index() => 
+    }
+}
